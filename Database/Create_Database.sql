7bb51f1c8a96d454998bcfb35c90f0b59014b272
ColasJ <janssen.colas@gmail.com>
2024-10-21 13:55:18 +0200

    creation basic database

--- Database/Create_Database.sql
+++ Database/Create_Database.sql
@@ -1,28 +1,29 @@
 CONNECT xter;
 
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    is_admin TINYINT(1) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    picture_id INT
+);
+
+INSERT INTO users (id, username, is_admin, email, user_password) VALUES (1, "coco", 1, "coco@lala.s", "1212");
+
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    content INT NOT NULL,
+    content VARCHAR(511) NOT NULL,
     date_post TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     reactions_id INT,
     picture_id INT,
     parent_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (reactions_id) REFERENCES reactions(id),
-    FOREIGN KEY (picture_id) REFERENCES pictures(id),
-    FOREIGN KEY (parent_id) REFERENCES messages(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL
-    is_admin BOOLEAN NOT NULL,
-    email INT NOT NULL,
-    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_password VARCHAR(255) NOT NULL,
-    picture_id INT,
-    follow_id INT,
-    FOREIGN KEY (picture_id) REFERENCES pictures(id),
-    FOREIGN KEY (follow_id) REFERENCES messages(id), 
-)
+INSERT INTO messages (user_id, content) VALUES (1, "bonjour");
+
+
