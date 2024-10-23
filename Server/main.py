90cba228ff7991c06e39eb6639f326b510e64515
Matthieu <mdufour.dev@gmail.com>
2024-10-23 14:19:21 +0200

    password hash

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# Importing all the routes from routes/__init__.py
 from routes import *
 
 app = FastAPI()
@@ -13,5 +14,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(register_router, prefix="/register")
-app.include_router(message_router, prefix="/messages")
+app.include_router(register_router, prefix="/register", tags=["Register"])
+app.include_router(message_router, prefix="/messages", tags=["Messages"])
--- Server/routes/register_route.py
+++ Server/routes/register_route.py
@@ -2,17 +2,20 @@ from fastapi import APIRouter
 from models import *
 import database_connect
 from mysql.connector import Error
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["Bcrypt"], deprecated="auto")
 
 router = APIRouter()
 
 @router.post("")
-def register_user(user: UserCreate):
+def register_user(user: UserCreate):    
     try:
         connection = database_connect.get_db_connection()
         cursor = connection.cursor()
         cursor.execute(
             "INSERT INTO user (username, is_admin, email, user_password, picture_id) VALUES (%s, 0, %s, %s, 0)",
-            (user.username, user.email, user.password)
+            (user.username, user.email, pwd_context.hash(user.password))
         )
         connection.commit()
         return {"message": "User registration successful!"}
