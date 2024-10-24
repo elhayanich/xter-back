d27345e8653b859eb37494354e15cf27801d6bbf
Matthieu <mdufour.dev@gmail.com>
2024-10-24 10:03:55 +0200

    accept merge

--- Server/database_connect.py
+++ Server/database_connect.py
@@ -3,8 +3,8 @@ import mysql.connector
 # Configuration de la connexion à la base de données
 # UNE FOIS CONFIGUREE, AJOUTER CE FICHIER A .GITIGNORE
 db_config = {
-    'user': 'chaymae',
-    'password': 'chay',
+    'user': 'root',
+    'password': 'devia',
     'host': 'localhost',
     'database': 'xter'
 }
