0221aad9a566a0e8d8adb67cc2e3aaaae71f2066
Chaymae El Hayani <layani.chay@gmail.com>
2024-10-29 10:13:52 +0100

    feat: Reply to a message

--- Client/src/components/Message.js
+++ Client/src/components/Message.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ReactMarkdown from "react-markdown";
+import Reply from './reply';
 
 const Message = () => {
     const [messages, setMessages] = useState([]);  
+    const [replyTo, setReplyTo] = useState(null);  
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -20,6 +22,10 @@ const Message = () => {
         fetchMessages();
     }, []);
 
+    // Fonction pour gérer la soumission de la réponse
+    const handleReplySubmit = (parentId, content) => {
+    };
+
     return (
         <div className="flex justify-center items-center">
             <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md mb-4">
@@ -34,6 +40,12 @@ const Message = () => {
                                 <ReactMarkdown>{message.content}</ReactMarkdown></p>
                                 <p><em>Posté le : {new Date(message.date_post).toLocaleString()}</em></p>
                                 <p className="mt-2"><strong>Tags:</strong> {message.tags ? message.tags.split(',').map(tag => <span key={tag} className="text-blue-500 mr-2">{tag.trim()}</span>) : "Aucun tag"}</p>
+                                
+                                <button onClick={() => setReplyTo(message.id)} className="text-blue-500 mt-2">Répondre</button>
+                                
+                                {replyTo === message.id && (
+                                    <Reply parentId={message.id} onSubmit={handleReplySubmit} />
+                                )}
                             </li>
                         ))
                     ) : (
