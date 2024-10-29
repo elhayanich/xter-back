eab53e3580a1208750fcf79309a79018d02a1738
unknown <ioana.acu@gmail.com>
2024-10-29 10:33:05 +0100

    addign before pull

--- Server/routes/user_route.py
+++ Server/routes/user_route.py
@@ -10,9 +10,7 @@ def get_user_from_db(user_id: int):
     connection = database_connect.get_db_connection()
     cursor = connection.cursor()
 
-    # try
     try: 
-        #requête sql
         cursor.execute("SELECT * from user where id = %s;", (user_id,))
         user_data = cursor.fetchone()
 
@@ -30,6 +28,38 @@ def get_user_from_db(user_id: int):
             )
         
         return user
+    
+    except Error as e:
+        print(f"L'erreur suivante est survenue : '{e}'")
+        raise HTTPException(status_code=500, detail="Erreur de connexion à la base de données")
+    
+    finally:
+        cursor.close()
+        connection.close() 
+
+@router.get("/{picture}", response_model=User)
+def get_user_picture_from_db(user_id: int):
+    connection = database_connect.get_db_connection()
+    cursor = connection.cursor()
+
+    try: 
+        cursor.execute("SELECT picture from user where id = %s;", (user_id,))
+        user_data = cursor.fetchone()
+
+        if user_data is None:
+            raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+
+        # récupérer les données sous forme d'User
+        user = User(
+                id = user_data[0],
+                username = user_data[1],
+                is_admin = user_data[2],
+                email = user_data[3],
+                date_inscription = user_data[5],
+                picture = user_data[6]
+            )
+        #retourner la photo uniquement
+        return user.picture
     
     except Error as e:
         print(f"L'erreur suivante est survenue : '{e}'")
