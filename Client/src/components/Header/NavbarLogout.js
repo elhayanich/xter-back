4d2f9031c16fbfcd4403106b3758ef5caf43e30a
TraoJok <jokerwayne104@gmail.com>
2024-10-31 10:20:45 +0100

    deconnexion home page

---
+++ Client/src/components/Header/NavbarLogout.js
@@ -0,0 +1,34 @@
+import React from "react"
+//import "./Navbar_admin.css"
+import Logo from "../../assets/images/logo.svg"
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const NavbarLogout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+      localStorage.removeItem("token"); // Supprime le token du stockage local
+      navigate('/login'); // Redirige vers la page de login
+  };
+
+  return (
+      <nav className="navbar">
+          <div className="navbar--logo-holder">
+              <img src={Logo} alt="logo" className="navbar--logo" />
+              <h1>Xter</h1>
+          </div>
+          <div>
+              <button 
+                  onClick={handleLogout} 
+                  className="w-full bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600 transition"
+              >
+                  Déconnexion
+              </button>
+             
+          </div>
+      </nav>
+  );
+}
+
+export default NavbarLogout;
--- Client/src/pages/homePage.js
+++ Client/src/pages/homePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MessageInput from '../components/messageInput';
-import Message from '../components/Message'; 
+import Message from '../components/Message';
+import NavbarLogout from '../components/Header/NavbarLogout';
 
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
         <div className="max-w-2xl mx-auto p-4">
             <MessageInput />
             <Message />
+            <NavbarLogout/>
         </div>
     );
 };
