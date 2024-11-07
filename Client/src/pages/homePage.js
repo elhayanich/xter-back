800c7625ace6c003c01e30ba4ae88cd48f8422e2
unknown <ioana.acu@gmail.com>
2024-11-07 14:09:38 +0100

    push before merge

--- Client/src/pages/homePage.js
+++ Client/src/pages/homePage.js
@@ -2,11 +2,14 @@ import React from 'react';
 import MessageInput from '../components/messageInput';
 import Message from '../components/Message';
 import NavbarLogout from '../components/Header/NavbarLogout';
+import FollowFilter from '../components/followerFilter';
 
 
 const HomePage = () => {
     return (
         <div className="max-w-2xl mx-auto p-4">
+            {/*Filtre en cours de test*/}
+            <FollowFilter />
             <MessageInput />
             <Message />
             <NavbarLogout/>
